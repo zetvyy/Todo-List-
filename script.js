6b1f2e72f240cf7d95ed341fabd84e9a8c216108
zetvyy <zetvyy@gmail.com>
2021-09-24 10:08:57 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,147 @@
+// Selector
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const todoFilter = document.querySelector(".todo-filter");
+
+// Event Listener
+document.addEventListener("DOMContentLoaded", getTodos);
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+todoFilter.addEventListener("click", filterTodo);
+
+// Function
+function addTodo(event) {
+  event.preventDefault();
+  // create todo div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  // create todo list
+  const newTodo = document.createElement("li");
+  newTodo.classList.add("todo-item");
+  newTodo.innerText = todoInput.value;
+  todoDiv.appendChild(newTodo);
+
+  // create CHECK Button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-square-fill" viewBox="0 0 16 16">
+                                <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z"/>
+                              </svg>`;
+  completedButton.classList.add("complete-btn");
+  todoDiv.append(completedButton);
+  // create ERASE button
+  const eraseButton = document.createElement("button");
+  eraseButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                              <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                            </svg>`;
+  eraseButton.classList.add("erase-btn");
+  todoDiv.append(eraseButton);
+  // Append todolist
+  todoList.append(todoDiv);
+  // ADD TODOS TO LOCAL STORAGE
+  savLocalTodos(todoInput.value);
+  // delete input value
+  todoInput.value = "";
+}
+
+function deleteCheck(e) {
+  const item = e.target;
+  // DELETE todo
+  if (item.classList[0] === "erase-btn") {
+    const todo = item.parentElement;
+    removeLocalTodos(todo);
+    todo.remove();
+  }
+  // CHECK MARK
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
+}
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  todos.forEach(todo => {
+    switch (e.target.value) {
+      case "All":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+    }
+  });
+}
+
+function savLocalTodos(todo) {
+  // CHECK Do I have already thing in here?
+  let todos;
+  if (localStorage.getItem("todos") == null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function getTodos(todo) {
+  // CHECK Do I have already thing in here?
+  let todos;
+  if (localStorage.getItem("todos") == null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.forEach(todo => {
+    // create todo div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    // create todo list
+    const newTodo = document.createElement("li");
+    newTodo.classList.add("todo-item");
+    newTodo.innerText = todo;
+    todoDiv.appendChild(newTodo);
+
+    // create CHECK Button
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-square-fill" viewBox="0 0 16 16">
+                                  <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z"/>
+                                </svg>`;
+    completedButton.classList.add("complete-btn");
+    todoDiv.append(completedButton);
+    // create ERASE button
+    const eraseButton = document.createElement("button");
+    eraseButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                              </svg>`;
+    eraseButton.classList.add("erase-btn");
+    todoDiv.append(eraseButton);
+    // Append todolist
+    todoList.append(todoDiv);
+  });
+}
+
+function removeLocalTodos(todo) {
+  // CHECK Do I have already thing in here?
+  let todos;
+  if (localStorage.getItem("todos") == null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  const todoIndex = todo.children[0].innerText;
+  todos.splice(todos.indexOf(todoIndex), 1);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
